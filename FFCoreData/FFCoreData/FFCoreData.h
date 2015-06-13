89be5173a91cb3407d072435584e3a81095de130
Florian Friedrich <ffried@me.com>
2015-06-13 16:02:42 +0200

    Converted FindAndOrCreate NSManagedObject extension to Swift 2.0

--- FFCoreData/FFCoreData/FFCoreData.h
+++ FFCoreData/FFCoreData/FFCoreData.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double FFCoreDataVersionNumber;
 
 //! Project version string for FFCoreData.
 FOUNDATION_EXPORT const unsigned char FFCoreDataVersionString[];
-
-#import <FFCoreData/NSManagedObject+FFCDFindAndOrCreate.h>
