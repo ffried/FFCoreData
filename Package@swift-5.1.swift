d0eaa4f6d8654c5a0191797f9eac11245e472853
Florian Friedrich <ffried@me.com>
2020-03-25 11:10:27 +0100

    Add support for Swift 5.2

--- Package@swift-5.1.swift
+++ Package@swift-5.1.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ffried/FFFoundation.git", from: "7.0.2"),
+        .package(url: "https://github.com/ffried/FFFoundation.git", from: "7.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
