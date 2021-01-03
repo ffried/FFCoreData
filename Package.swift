30dd17f5726d343f6fe337f6961a44f815ff01e8
Florian Friedrich <ffried@me.com>
2021-01-03 17:14:13 +0100

    Update FFFoundation

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ffried/FFFoundation.git", from: "8.1.0"),
+        .package(url: "https://github.com/ffried/FFFoundation.git", from: "9.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
