42744ee49eee8d369c15d7843372f22b41351597
Florian <1689782+ffried@users.noreply.github.com>
2026-03-25 12:54:17 +0100

    Skip testing Swift 6.0 due to platform issues

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/ffried/FFFoundation", from: "9.7.0"),
+        .package(url: "https://github.com/ffried/FFFoundation", from: "9.8.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
